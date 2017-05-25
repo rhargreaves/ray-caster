ca9e5a91f6bf94f48ef1b43d3a14b19295c223a0
rhargreaves <robert@roberthargreaves.net>
2017-05-25 16:35:38 +0100

    Add game loop

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+import pygame
 
-from graphics import *
-win = GraphWin()
+pygame.init()
+pygame.display.set_caption("ray-caster")
+
+WHITE = (0, 0, 0)
+
+size = (800, 600)
+screen = pygame.display.set_mode(size)
+
+done = False
+clock = pygame.time.Clock()
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill(WHITE)
+
+    pygame.display.flip()
+    clock.tick(60)
